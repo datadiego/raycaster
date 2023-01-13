1bae20bc9c62b36fe29cc2635dd60307013d90af
Juan Diego <juandiegomariscal@gmail.com>
2023-01-13 14:06:49 +0100

    feat 👍 creamos la clase particle, proyectamos alrededor del pj

---
+++ particle.js
@@ -0,0 +1,25 @@
+class Particle{
+    constructor(){
+        this.pos = createVector(width/2, height/2);
+        this.rays = [];
+        for(let a = 0; a < 360; a += 10){
+            this.rays.push(new Ray(this.pos, radians(a)));
+        }
+    }
+    show(){
+        fill(255);
+        ellipse(this.pos.x, this.pos.y, 4);
+        for(let ray of this.rays){
+            ray.show();
+        }
+    }
+    look(wall){
+        for(let ray of this.rays){
+            const pt = ray.cast(wall);
+            if(pt){
+                fill(255);
+                ellipse(pt.x, pt.y, 8, 8);
+            }
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,23 +1,27 @@
 let wall
 let ray
-
+let particle
 function setup() {
   createCanvas(400, 400);
   wall = new Boundary(300, 100, 200, 300);
   ray = new Ray(100, 200);
+  particle = new Particle();
 
 }
 
 function draw() {
   background(0);
   wall.show();
-  ray.show();
-  ray.lookAt(mouseX, mouseY);
-  let pt = ray.cast(wall)
-  if(pt){
-    fill(255);
-    ellipse(pt.x, pt.y, 8, 8);
+  particle.show();
+  particle.look(wall);
+  // ray.show();
+  // ray.lookAt(mouseX, mouseY);
+  // let pt = ray.cast(wall)
+  // if(pt){
+  //   fill(255);
+  //   ellipse(pt.x, pt.y, 8, 8);
     
-    console.log(pt)
-  }
+  //   console.log(pt)
+  // }
+
 }
