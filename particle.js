ad2e0fa5e1bc60e0961827470b15f8b80481b2d7
Juan Diego <juandiegomariscal@gmail.com>
2023-01-13 15:50:39 +0100

    feat 👍 el rayo solo se proyecta al muro mas próximo

--- particle.js
+++ particle.js
@@ -2,7 +2,7 @@ class Particle{
     constructor(){
         this.pos = createVector(width/2, height/2);
         this.rays = [];
-        for(let a = 0; a < 360; a += 10){
+        for(let a = 0; a < 360; a += 1){
             this.rays.push(new Ray(this.pos, radians(a)));
         }
     }
@@ -19,12 +19,24 @@ class Particle{
     }
     look(wall){
         for(let ray of this.rays){
-            const pt = ray.cast(wall);
-            if(pt){
-                fill(255);
-                line(this.pos.x, this.pos.y, pt.x, pt.y)
-                ellipse(pt.x, pt.y, 8, 8);
+            let closest = null;
+            let record = Infinity;
+            for(let wall of walls){
+                const pt = ray.cast(wall);
+                if(pt){
+                const distance = p5.Vector.dist(this.pos, pt);
+                if (distance < record){
+                    record = distance;
+                    closest = pt;
+                    }
+
+                }
+            }
+            if(closest){
+                stroke(255, 100);
+                line(this.pos.x, this.pos.y, closest.x, closest.y);
             }
         }
     }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,27 @@
-let wall
+let walls = []
 let ray
 let particle
 function setup() {
-  createCanvas(400, 400);
-  wall = new Boundary(300, 100, 200, 300);
-  ray = new Ray(100, 200);
+  createCanvas(720, 480);
+  for(let i = 0; i < 5; i++){
+    let x1 = random(width);
+    let x2 = random(width);
+    let y1 = random(height);
+    let y2 = random(height);
+    walls[i] = new Boundary(x1, y1, x2, y2);
+  }
   particle = new Particle();
 
 }
 
 function draw() {
   background(0);
-  wall.show();
+  for (let wall of walls){
+    wall.show();
+  }
   particle.update(mouseX, mouseY)
   particle.show();
-  particle.look(wall);
+  particle.look(walls);
   // ray.show();
   // ray.lookAt(mouseX, mouseY);
   // let pt = ray.cast(wall)
