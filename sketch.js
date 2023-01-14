bdcefb84bab5377f55f472ed2db181f5913f8d98
Juan Diego <juandiegomariscal@gmail.com>
2023-01-14 11:31:10 +0100

    feat 👍 muros en los bordes y movimiento automatico

--- sketch.js
+++ sketch.js
@@ -5,16 +5,24 @@ let particle
 let xoff = 0;
 let yoff = 100;
 function setup() {
-  createCanvas(720, 480);
+
+  createCanvas(displayWidth, displayHeight);
   for(let i = 0; i < 5; i++){
     let x1 = random(width);
     let x2 = random(width);
     let y1 = random(height);
     let y2 = random(height);
     walls[i] = new Boundary(x1, y1, x2, y2);
+    walls[i].move()
   }
+  console.log(width, height)
+  console.log("tengo " + walls.length + " paredes")
+  walls.push(new Boundary(0, 0, width, 0));
+  walls.push(new Boundary(width, 0, width, height));
+  walls.push(new Boundary(width, height, 0, height));
+  walls.push(new Boundary(0, height, 0, 0));
+  console.log("tengo " + walls.length + " paredes")
   particle = new Particle();
-
 }
 
 function draw() {
