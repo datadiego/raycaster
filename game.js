7a6e3beafd5301dea1191481c62fa248c7fb58dd
Juan Diego <juandiegomariscal@gmail.com>
2023-01-25 11:09:36 +0100

    feat: 👍ahora los emojis siempre salen centrados

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 const FPS = 30;
 let game = null;
 class Game{
-    constructor(window){
+    constructor(cells){
         this.level = new Level(8,8);
-        this.window = window; //array con las lineas de texto para los emoji
         this.turno_jugador = true;
+        this.grid = cells;
     }
     teclado(key){
         if(key == 38){
@@ -24,17 +24,18 @@ class Game{
         for(let i = 0; i < this.level.map.length; i++){
             for(let j = 0; j < this.level.map[i].length; j++){
                 if(this.level.map[i][j] == 0){
-                    this.window[i].innerHTML += "⬛";
+                    this.grid[i][j].innerHTML = "🟫";
+                    
                 }else if(this.level.map[i][j] == 1){
-                    this.window[i].innerHTML += "🔳";
+                    this.grid[i][j].innerHTML = "🌳";
                 }else if(this.level.map[i][j] == 2){
-                    this.window[i].innerHTML += "🕳️";
+                    this.grid[i][j].innerHTML = "🕳️";
                 }
                 else if (this.level.map[i][j] == 3){
-                    this.window[i].innerHTML += "🧙🏻‍♂️";
+                    this.grid[i][j].innerHTML = "🧙🏻‍♂️";
                 }
                 else if (this.level.map[i][j] == 4){
-                    this.window[i].innerHTML += "🐀";
+                    this.grid[i][j].innerHTML = "🐀";
                 }
             }
         }
@@ -59,10 +60,86 @@ function inicializa(){
     let canvas5 = document.getElementById('canvas5');
     let canvas6 = document.getElementById('canvas6');
     let canvas7 = document.getElementById('canvas7');
+
+    let square1 = document.getElementById('square-1')
+    let square2 = document.getElementById('square-2')
+    let square3 = document.getElementById('square-3')
+    let square4 = document.getElementById('square-4')
+    let square5 = document.getElementById('square-5')
+    let square6 = document.getElementById('square-6')
+    let square7 = document.getElementById('square-7')
+    let square8 = document.getElementById('square-8')
+    let square9 = document.getElementById('square-9')
+    let square10 = document.getElementById('square-10')
+    let square11 = document.getElementById('square-11')
+    let square12 = document.getElementById('square-12')
+    let square13 = document.getElementById('square-13')
+    let square14 = document.getElementById('square-14')
+    let square15 = document.getElementById('square-15')
+    let square16 = document.getElementById('square-16')
+    let square17 = document.getElementById('square-17')
+    let square18 = document.getElementById('square-18')
+    let square19 = document.getElementById('square-19')
+    let square20 = document.getElementById('square-20')
+    let square21 = document.getElementById('square-21')
+    let square22 = document.getElementById('square-22')
+    let square23 = document.getElementById('square-23')
+    let square24 = document.getElementById('square-24')
+    let square25 = document.getElementById('square-25')
+    let square26 = document.getElementById('square-26')
+    let square27 = document.getElementById('square-27')
+    let square28 = document.getElementById('square-28')
+    let square29 = document.getElementById('square-29')
+    let square30 = document.getElementById('square-30')
+    let square31 = document.getElementById('square-31')
+    let square32 = document.getElementById('square-32')
+    let square33 = document.getElementById('square-33')
+    let square34 = document.getElementById('square-34')
+    let square35 = document.getElementById('square-35')
+    let square36 = document.getElementById('square-36')
+    let square37 = document.getElementById('square-37')
+    let square38 = document.getElementById('square-38')
+    let square39 = document.getElementById('square-39')
+    let square40 = document.getElementById('square-40')
+    let square41 = document.getElementById('square-41')
+    let square42 = document.getElementById('square-42')
+    let square43 = document.getElementById('square-43')
+    let square44 = document.getElementById('square-44')
+    let square45 = document.getElementById('square-45')
+    let square46 = document.getElementById('square-46')
+    let square47 = document.getElementById('square-47')
+    let square48 = document.getElementById('square-48')
+    let square49 = document.getElementById('square-49')
+    let square50 = document.getElementById('square-50')
+    let square51 = document.getElementById('square-51')
+    let square52 = document.getElementById('square-52')
+    let square53 = document.getElementById('square-53')
+    let square54 = document.getElementById('square-54')
+    let square55 = document.getElementById('square-55')
+    let square56 = document.getElementById('square-56')
+    let square57 = document.getElementById('square-57')
+    let square58 = document.getElementById('square-58')
+    let square59 = document.getElementById('square-59')
+    let square60 = document.getElementById('square-60')
+    let square61 = document.getElementById('square-61')
+    let square62 = document.getElementById('square-62')
+    let square63 = document.getElementById('square-63')
+    let square64 = document.getElementById('square-64')
+
+    let squares = [
+        [square1,square2,square3,square4,square5,square6,square7,square8],
+        [square9,square10,square11,square12,square13,square14,square15,square16],
+        [square17,square18,square19,square20,square21,square22,square23,square24],
+        [square25,square26,square27,square28,square29,square30,square31,square32],
+        [square33,square34,square35,square36,square37,square38,square39,square40],
+        [square41,square42,square43,square44,square45,square46,square47,square48],
+        [square49,square50,square51,square52,square53,square54,square55,square56],
+        [square57,square58,square59,square60,square61,square62,square63,square64]
+    ]
     
     let window = [canvas0,canvas1,canvas2,canvas3,canvas4,canvas5,canvas6,canvas7];
     
-    let game = new Game(window);
+    let game = new Game(squares);
     setInterval(function(){
         borrarCanvas()
         game.render();
